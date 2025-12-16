ab402e76e5320242fd51687c55a1e9add2e34c17
Qiqi Jin <ginqi7@gmail.com>
2025-12-16 10:50:55 +0800

    Add base URL and use relative paths.

--- _data/project.js
+++ _data/project.js
@@ -1,11 +1,11 @@
 export default function () {
-    return {
-	"currentYear": new Date().getFullYear(),
-	"title": "Hello World",
-	"author": "Qiqi Jin",
-	"mail": "ginqi7@gmail.com",
-	"github": "https://github.com/ginqi7",
-	"indexPostSize" : 5
-    }
-};
-
+  return {
+    currentYear: new Date().getFullYear(),
+    title: "Hello World",
+    author: "Qiqi Jin",
+    mail: "ginqi7@gmail.com",
+    github: "https://github.com/ginqi7",
+    indexPostSize: 5,
+    baseURL: "/",
+  };
+}
